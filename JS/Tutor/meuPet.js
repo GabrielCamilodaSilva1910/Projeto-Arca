71c046217c8de2b069a7c85396114deb83c8cff2
Gabriel Camilo da Silva <gabrielcamilosilva11@gmail.com>
2026-06-09 18:18:46 -0300

    Add files via upload
    Função de cadastrar pet funcionando 100%

--- JS/Tutor/meuPet.js
+++ JS/Tutor/meuPet.js
@@ -1,6 +1,21 @@
 // pega o perfil definido na tela de login
 let perfilAtivo = JSON.parse(sessionStorage.getItem("perfilAtivo"));
 
+var PETS = [
+  { p_id: 1, p_nomePet: 'Bolinha', p_especie: 'Cão', p_raca: 'Vira-lata', p_idade: '2 anos', p_sexo: 'Macho', p_peso: '8 kg', p_castrado: 'Não', p_foto: '🐶' }
+];
+
+const MENUS = {
+  tutor: [
+    { id: 'home', local: '../home.html', icone: '🏠', label: 'Início' },
+    { id: 'meu-pet', local: 'meuPet.html', icone: '🐶', label: 'Meu Pet' },
+    { id: 'castracaoT', local: 'castracaoT', icone: '✂️', label: 'Solicitar Castração' },
+    { id: 'resgateT', local: 'resgateT', icone: '🚨', label: 'Solicitar Resgate' },
+  ],
+};
+
+
+
 // define o laouyt de acordo com o perfi do usuario
 document.getElementById('perfil').textContent = perfilAtivo.perfil.toUpperCase();
 
@@ -16,14 +31,7 @@ function sair() {
 }
 
 // obejeto de cada menu de acordo com o perfil
-const MENUS = {
-  tutor: [
-    { id: 'home', local: '../home.html', icone: '🏠', label: 'Início' },
-    { id: 'meu-pet', local: 'meuPet.html', icone: '🐶', label: 'Meu Pet' },
-    { id: 'castracaoT', local: 'castracaoT', icone: '✂️', label: 'Solicitar Castração' },
-    { id: 'resgateT', local: 'resgateT', icone: '🚨', label: 'Solicitar Resgate' },
-  ],
-};
+
 
 // função que cria o menu da lateral de acordo com cada perfil
 function construirMenu() {
@@ -36,9 +44,44 @@ function construirMenu() {
   ).join('');
 }
 
+document.addEventListener("DOMContentLoaded", construirConteudo);
 
+function construirConteudo() {
+  const conteudo = document.getElementById('conteudo');
+  const itens = PETS || [];
+  const htmlCards = itens.map(i =>
+    `
+    <div class="card" id="${i.p_id}">
+      <div class="card-topo">
+          <div class="card-foto">${i.p_foto}</div>
+          <div>
+              <h3>${i.p_nomePet}</h3>
+              <span class="tag tag-verde">${i.p_especie}</span>
+              <span class="tag tag-azul">${i.p_raca}</span>
+          </div>
+      </div>
+      <div class="cad-corpo">
+          <div><label>Idade</label>
+              <p>${i.p_idade}</p>
+          </div>
+          <div><label>Sexo</label>
+              <p>${i.p_sexo}</p>
+          </div>
+          <div><label>Peso</label>
+              <p>${i.p_peso}</p>
+          </div>
+          <div><label>Castrado?</label>
+              <p>${i.p_castrado}</p>
+          </div>
+      </div>
+    </div>  
+    `
+  ).join('');
 
-
+  conteudo.innerHTML = htmlCards + `
+    <button class="btn-cadastro" onclick="abrirCadastroPet()">+ Cadastrar novo pet</button>
+  `;
+}
 
 
 
@@ -46,6 +89,67 @@ function construirMenu() {
 function abrirCadastroPet() {
   document.getElementById('fundo-cadastro').classList.toggle('aberto');
   document.getElementById('menu-lateral').classList.remove('aberto');
+
+}
+
+
+
+
+
+
+
+
+
+function salvarCadastroPet() {
+
+
+
+  var nomePet = document.getElementById('nomePet').value;
+  var especie = document.getElementById('especie').value;
+  var raca = document.getElementById('raca').value;
+  var idade = document.getElementById('idade').value;
+  var sexo = document.getElementById('sexo').value;
+  var peso = document.getElementById('peso').value;
+  var castrado = document.querySelector('input[name="opcao"]:checked').value;
+
+  if (nomePet != "" && especie != "" && raca != "" && idade != "" && sexo != "" && peso != "" && castrado != "") {
+
+
+    if (especie == "Cão") {
+      var foto = '🐶';
+    } else {
+      var foto = '🐱';
+    }
+
+    const novoPet = {
+      p_id: PETS.length + 1,
+      p_nomePet: nomePet,
+      p_especie: especie,
+      p_raca: raca,
+      p_idade: idade,
+      p_sexo: sexo,
+      p_peso: peso,
+      p_castrado: castrado,
+      p_foto: foto
+    };
+
+    PETS.push(novoPet);
+
+    alert(`Novo Pet Adicionado: ${novoPet.p_nomePet}\n`);
+
+
+    construirConteudo()
+
+    fecharCadastroPet();
+
+
+
+  } else {
+    alert("Por favor preencher todos os campos para realizar o cadastro.")
+  }
+
+
+
 
 }
 
