d05a3c12db53043b256f70c19942b0418700bf35
Gabriel Camilo da Silva <gabrielcamilosilva11@gmail.com>
2026-06-11 10:16:39 -0300

    Add files via upload

--- JS/Login.js
+++ JS/Login.js
@@ -1,13 +1,43 @@
 const USUARIOS = {
-  tutor:      { senha: '123456',   perfil: 'tutor'},
-  candidato:  { senha: 'cand!098', perfil: 'candidato'},
-  Ong:        { senha: 'ong$-135', perfil: 'ong'},
-  prefeitura: { senha: 'pref@456', perfil: 'prefeitura'}
+  tutor: { senha: '123456', perfil: 'tutor' },
+  candidato: { senha: 'cand!098', perfil: 'candidato' },
+  Ong: { senha: 'ong$-135', perfil: 'ong' },
+  prefeitura: { senha: 'pref@456', perfil: 'prefeitura' }
 };
 
 
 let perfilAtivo = null;
 
+
+
+document.addEventListener("DOMContentLoaded", PetsSessao);
+
+
+
+var PETS
+
+function PetsSessao() {
+
+  
+  var Pet_sessao = sessionStorage.getItem("PETS");
+
+  if (Pet_sessao) {
+
+
+    PETS = JSON.parse(Pet_sessao);
+    
+
+  } else {
+
+    PETS = [
+      { p_id: 0, p_nomePet: 'Bolinha', p_especie: 'Cão', p_raca: 'Vira-lata', p_idade: '2 anos', p_sexo: 'Macho', p_peso: '8 kg', p_castrado: 'Não', p_foto: '🐶' }
+    ];
+    
+
+  };
+  
+  
+}
 
 
 
@@ -18,9 +48,10 @@ function realizarLogin() {
   if (USUARIOS[usuario] && USUARIOS[usuario].senha === senha) {
     perfilAtivo = USUARIOS[usuario];
     sessionStorage.setItem("perfilAtivo", JSON.stringify(perfilAtivo));
+    sessionStorage.setItem("PETS", JSON.stringify(PETS));
     window.location.href = "./Telas/home.html";
 
-    
+
   } else {
     alerta.classList.remove('esconde');
     document.getElementById('inp-senha').value = '';
--- JS/Tutor/meuPet.js
+++ JS/Tutor/meuPet.js
@@ -1,9 +1,13 @@
 // pega o perfil definido na tela de login
 let perfilAtivo = JSON.parse(sessionStorage.getItem("perfilAtivo"));
 
-var PETS = [
-  { p_id: 1, p_nomePet: 'Bolinha', p_especie: 'Cão', p_raca: 'Vira-lata', p_idade: '2 anos', p_sexo: 'Macho', p_peso: '8 kg', p_castrado: 'Não', p_foto: '🐶' }
-];
+
+let PETS = JSON.parse(sessionStorage.getItem("PETS"));
+
+// sessionStorage.setItem("PETS", JSON.stringify(PETS));
+
+
+
 
 const MENUS = {
   tutor: [
@@ -13,6 +17,8 @@ const MENUS = {
     { id: 'resgateT', local: 'resgateT', icone: '🚨', label: 'Solicitar Resgate' },
   ],
 };
+
+var petIdSelecionado = null;
 
 
 
@@ -29,6 +35,7 @@ function sair() {
   perfilAtivo = null;
   window.location.href = "../../index.html";
 }
+
 
 // obejeto de cada menu de acordo com o perfil
 
@@ -52,6 +59,10 @@ function construirConteudo() {
   const htmlCards = itens.map(i =>
     `
     <div class="card" id="${i.p_id}">
+          <div style="display:flex; justify-content: end;">
+            <button class="btn-altera" onclick="abrirAlteraPet(${i.p_id})"><img src="../../Imagens/Icones/lapis.png" alt="Botão de alterar"></button>
+            <button class="btn-exclui" onclick="excluirCadastroPet(${i.p_id})"><img src="../../Imagens/Icones/lixeira.png" alt="Botão de excluir"></button>
+          </div>
       <div class="card-topo">
           <div class="card-foto">${i.p_foto}</div>
           <div>
@@ -60,7 +71,7 @@ function construirConteudo() {
               <span class="tag tag-azul">${i.p_raca}</span>
           </div>
       </div>
-      <div class="cad-corpo">
+      <div class="card-corpo">
           <div><label>Idade</label>
               <p>${i.p_idade}</p>
           </div>
@@ -87,33 +98,77 @@ function construirConteudo() {
 
 
 function abrirCadastroPet() {
-  document.getElementById('fundo-cadastro').classList.toggle('aberto');
+  document.getElementById('fundo_cadastro').classList.toggle('aberto');
   document.getElementById('menu-lateral').classList.remove('aberto');
+}
+
+
+function abrirAlteraPet(id) {
+
+  petIdSelecionado = id;
+
+  document.getElementById('fundo_altera').classList.toggle('aberto');
+  document.getElementById('menu-lateral').classList.remove('aberto');
+
+
+  var posicao = 0;
+
+  while (PETS[posicao].p_id != id) {
+
+    posicao++;
+  };
+
+
+  var pet = PETS[posicao];
+
+
+  document.getElementById('AlteraNomePet').value = pet.p_nomePet;
+  document.getElementById('AlteraEspecie').value = pet.p_especie;
+  document.getElementById('AlteraRaca').value = pet.p_raca;
+  document.getElementById('AlteraIdade').value = pet.p_idade;
+  document.getElementById('AlteraSexo').value = pet.p_sexo;
+  document.getElementById('AlteraPeso').value = pet.p_peso;
+
+  if (pet.p_castrado == "Sim") {
+    document.getElementById("AlteraSim").checked = true;
+  } else {
+    document.getElementById("AlteraNao").checked = true;
+  }
 
 }
 
 
 
+function alterarCadastroPet(id) {
+
+  var posicao = 0;
+
+  while (PETS[posicao].p_id != id) {
+
+    posicao++;
+  };
 
 
+  var nomePet = document.getElementById('AlteraNomePet').value;
+  var especie = document.getElementById('AlteraEspecie').value;
+  var raca = document.getElementById('AlteraRaca').value;
+  var idade = document.getElementById('AlteraIdade').value;
+  var sexo = document.getElementById('AlteraSexo').value;
+  var peso = document.getElementById('AlteraPeso').value;
+  var inputCastrado = document.querySelector('input[name="AlteraOpcao"]:checked');
 
+  // alert('nome: ' + nomePet + '\n' +
+  //   'especie: ' + especie + '\n' +
+  //   'raca: ' + raca + '\n' +
+  //   'idade: ' + idade + '\n' +
+  //   'sexo: ' + sexo + '\n' +
+  //   'peso: ' + peso + '\n' +
+  //   'inputCastrado: ' + inputCastrado + '\n'
+  // );
 
+  if (nomePet != "" && especie != "" && raca != "" && idade != "" && sexo != "" && peso != "" && inputCastrado != null) {
 
-
-function salvarCadastroPet() {
-
-
-
-  var nomePet = document.getElementById('nomePet').value;
-  var especie = document.getElementById('especie').value;
-  var raca = document.getElementById('raca').value;
-  var idade = document.getElementById('idade').value;
-  var sexo = document.getElementById('sexo').value;
-  var peso = document.getElementById('peso').value;
-  var castrado = document.querySelector('input[name="opcao"]:checked').value;
-
-  if (nomePet != "" && especie != "" && raca != "" && idade != "" && sexo != "" && peso != "" && castrado != "") {
-
+    var castrado = inputCastrado.value;
 
     if (especie == "Cão") {
       var foto = '🐶';
@@ -121,8 +176,96 @@ function salvarCadastroPet() {
       var foto = '🐱';
     }
 
-    const novoPet = {
-      p_id: PETS.length + 1,
+    var Pet = {
+      p_id: id,
+      p_nomePet: nomePet,
+      p_especie: especie,
+      p_raca: raca,
+      p_idade: idade,
+      p_sexo: sexo,
+      p_peso: peso,
+      p_castrado: castrado,
+      p_foto: foto
+    };
+
+    PETS[posicao] = Pet;
+
+    sessionStorage.setItem("PETS", JSON.stringify(PETS));
+
+    alert(`Cadastro do Pet atelterado com Sucesso.`);
+
+
+    construirConteudo();
+
+    fecharAlteraPet();
+
+  } else {
+
+    alert(`Por favor preencha todos os campos.`);
+
+  }
+
+}
+
+
+
+
+function excluirCadastroPet(id) {
+
+  var indice = PETS.findIndex(pet => pet.p_id == id);
+
+
+  // 2. Verifica se o pet realmente foi encontrado (se é diferente de -1)
+  if (indice != -1) {
+
+    // 3. Remove 1 elemento exatamente na posição que encontramos
+    PETS.splice(indice, 1);
+
+    alert(`Pet removido com sucesso!`);
+
+    // 4. Atualiza a sua tela (chamando as funções que você já tem no código)
+    construirConteudo();
+
+  } else {
+    alert(`Erro: Pet com o ID ${id} não foi encontrado.`);
+  }
+
+}
+
+
+
+function salvarCadastroPet() {
+
+  var nomePet = document.getElementById('nomePet').value;
+  var especie = document.getElementById('especie').value;
+  var raca = document.getElementById('raca').value;
+  var idade = document.getElementById('idade').value;
+  var sexo = document.getElementById('sexo').value;
+  var peso = document.getElementById('peso').value;
+  var inputCastrado = document.querySelector('input[name="opcao"]:checked');
+
+  if (nomePet != "" && especie != "" && raca != "" && idade != "" && sexo != "" && peso != "" && inputCastrado != null) {
+
+    var castrado = inputCastrado.value;
+
+
+
+
+    if (PETS.length > 0) {
+      var ultimoId = PETS[(PETS.length - 1)].p_id;
+
+    } else {
+      var ultimoId = 0;
+    }
+
+    if (especie == "Cão") {
+      var foto = '🐶';
+    } else {
+      var foto = '🐱';
+    }
+
+    var novoPet = {
+      p_id: ultimoId + 1,
       p_nomePet: nomePet,
       p_especie: especie,
       p_raca: raca,
@@ -135,26 +278,29 @@ function salvarCadastroPet() {
 
     PETS.push(novoPet);
 
+    sessionStorage.setItem("PETS", JSON.stringify(PETS));
+
     alert(`Novo Pet Adicionado: ${novoPet.p_nomePet}\n`);
 
 
-    construirConteudo()
+    construirConteudo();
 
     fecharCadastroPet();
 
-
-
   } else {
-    alert("Por favor preencher todos os campos para realizar o cadastro.")
+
+    alert(`Por favor preencha todos os campos.`);
+
   }
-
-
 
 
 }
 
 
 function fecharCadastroPet() {
-  document.getElementById('fundo-cadastro').classList.remove('aberto');
+  document.getElementById('fundo_cadastro').classList.remove('aberto');
 }
 
+function fecharAlteraPet() {
+  document.getElementById('fundo_altera').classList.remove('aberto');
+}
